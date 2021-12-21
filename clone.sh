0a2b33eafe71514179e58bd3ccc6f9f471617cfc
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2021-12-21 18:57:32 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -12,18 +12,18 @@
      #rm -rf kernel/xiaomi/onclite 
    
    #replace source files
-     rm -rf device/qcom/sepolicy_vndr
-     rm -rf device/qcom/sepolicy-legacy-um
-     rm -rf device/qcom/sepolicy
-     rm -rf external/selinux
-     rm -rf system/sepolicy
+     #rm -rf device/qcom/sepolicy_vndr
+     #rm -rf device/qcom/sepolicy-legacy-um
+     #rm -rf device/qcom/sepolicy
+     #rm -rf external/selinux
+     #rm -rf system/sepolicy
      
    #clone files
-     git clone https://github.com/afterallafk/device_qcom_sepolicy_vndr.git -b 12 device/qcom/sepolicy_vndr
-     git clone https://github.com/afterallafk/device_qcom_sepolicy-legacy-um.git -b 12 device/qcom/sepolicy-legacy-um
-     git clone https://github.com/afterallafk/device_qcom_sepolicy.git -b 12 device/qcom/sepolicy
-     git clone https://github.com/afterallafk/android_external_selinux.git -b 12 external/selinux
-     git clone https://github.com/afterallafk/system_sepolicy.git -b 12 system/sepolicy
+     #git clone https://github.com/afterallafk/device_qcom_sepolicy_vndr.git -b 12 device/qcom/sepolicy_vndr
+     #git clone https://github.com/afterallafk/device_qcom_sepolicy-legacy-um.git -b 12 device/qcom/sepolicy-legacy-um
+     #git clone https://github.com/afterallafk/device_qcom_sepolicy.git -b 12 device/qcom/sepolicy
+     #git clone https://github.com/afterallafk/android_external_selinux.git -b 12 external/selinux
+     #git clone https://github.com/afterallafk/system_sepolicy.git -b 12 system/sepolicy
    
    #clone_HALS
      #git clone https://github.com/afterallafk/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
