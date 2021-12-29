0861944f3541f804dc530db5ccf27ce43f3cad99
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2021-12-29 20:15:13 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -7,12 +7,12 @@
      rm -rf hardware/qcom-caf/msm8996/media
    
    #remove_old_trees
-     rm -rf device/xiaomi/onclite
-     rm -rf vendor/xiaomi/onclite
-     rm -rf kernel/xiaomi/onclite 
+     #rm -rf device/xiaomi/onclite
+     #rm -rf vendor/xiaomi/onclite
+     #rm -rf kernel/xiaomi/onclite 
    
    #replace source files
-      rm -rf frameworks/base
+      #rm -rf frameworks/base
      #rm -rf device/qcom/sepolicy_vndr
      #rm -rf device/qcom/sepolicy-legacy-um
      #rm -rf device/qcom/sepolicy
@@ -20,7 +20,7 @@
      #rm -rf system/sepolicy
      
    #clone files
-      git clone https://github.com/afterallafk/voltage_fwb.git -b 12 frameworks/base
+      #git clone https://github.com/afterallafk/voltage_fwb.git -b 12 frameworks/base
      #git clone https://github.com/afterallafk/device_qcom_sepolicy_vndr.git -b 12 device/qcom/sepolicy_vndr
      #git clone https://github.com/afterallafk/device_qcom_sepolicy-legacy-um.git -b 12 device/qcom/sepolicy-legacy-um
      #git clone https://github.com/afterallafk/device_qcom_sepolicy.git -b 12 device/qcom/sepolicy
@@ -34,6 +34,6 @@
      git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b twelve hardware/qcom-caf/wlan
     
     #clone_trees
-     git clone https://github.com/afterallafk/voltage_device_xiaomi_onclite.git -b 12 device/xiaomi/onclite
-     git clone https://github.com/afterallafk/vendor_xiaomi_onclite-12.git -b twelve vendor/xiaomi/onclite
-     git clone https://github.com/afterallafk/kernel_xiaomi_onclite.git -b twelve kernel/xiaomi/onclite
+     #git clone https://github.com/afterallafk/voltage_device_xiaomi_onclite.git -b 12 device/xiaomi/onclite
+     #git clone https://github.com/afterallafk/vendor_xiaomi_onclite-12.git -b twelve vendor/xiaomi/onclite
+     #git clone https://github.com/afterallafk/kernel_xiaomi_onclite.git -b twelve kernel/xiaomi/onclite
