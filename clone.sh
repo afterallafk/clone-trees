de7b3dce69a906cab71167fdbeb631709406354b
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2021-12-30 19:18:59 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
   #remove_HALS
-     rm -rf hardware/qcom-caf/wlan
-     rm -rf hardware/qcom-caf/msm8996/audio
-     rm -rf hardware/qcom-caf/msm8996/display
-     rm -rf hardware/qcom-caf/msm8996/media
+     #rm -rf hardware/qcom-caf/wlan
+     #rm -rf hardware/qcom-caf/msm8996/audio
+     #rm -rf hardware/qcom-caf/msm8996/display
+     #rm -rf hardware/qcom-caf/msm8996/media
    
    #remove_old_trees
      rm -rf device/xiaomi/onclite
-     rm -rf vendor/xiaomi/onclite
-     rm -rf kernel/xiaomi/onclite 
+     #rm -rf vendor/xiaomi/onclite
+     #rm -rf kernel/xiaomi/onclite 
    
    #replace source files
      #rm -rf device/qcom/sepolicy_vndr
@@ -26,12 +26,12 @@
      #git clone https://github.com/afterallafk/system_sepolicy.git -b 12 system/sepolicy
    
    #clone_HALS
-     git clone https://github.com/afterallafk/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
-     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b twelve hardware/qcom-caf/msm8996/audio
-     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b twelve hardware/qcom-caf/msm8996/media
-     git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b twelve hardware/qcom-caf/wlan
+     #git clone https://github.com/afterallafk/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
+     #git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b twelve hardware/qcom-caf/msm8996/audio
+     #git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b twelve hardware/qcom-caf/msm8996/media
+     #git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b twelve hardware/qcom-caf/wlan
     
     #clone_trees
      git clone https://github.com/afterallafk/voltage_device_xiaomi_onclite.git -b 12 device/xiaomi/onclite
-     git clone https://github.com/afterallafk/vendor_xiaomi_onclite-12.git -b twelve vendor/xiaomi/onclite
-     git clone https://github.com/afterallafk/kernel_xiaomi_onclite.git -b twelve kernel/xiaomi/onclite
+     #git clone https://github.com/afterallafk/vendor_xiaomi_onclite-12.git -b twelve vendor/xiaomi/onclite
+     #git clone https://github.com/afterallafk/kernel_xiaomi_onclite.git -b twelve kernel/xiaomi/onclite
