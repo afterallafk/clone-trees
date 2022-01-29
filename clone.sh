dc0308bee5347dc55fe2e7af8ef56cafe831b486
Aditya Pratap Singh <87426352+afterallafk@users.noreply.github.com>
2022-01-29 16:46:04 +0530

    Update clone.sh

--- clone.sh
+++ clone.sh
@@ -12,11 +12,7 @@
      rm -rf kernel/xiaomi/onclite 
    
    #clone_HALS
-     git clone https://github.com/afterallafk/android_hardware_qcom_display.git -b lineage-18.1-caf-msm8996 hardware/qcom-caf/msm8996/display
-     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_audio.git -b twelve hardware/qcom-caf/msm8996/audio
-     git clone https://github.com/afterallafk/hardware_qcom-caf_msm8996_media.git -b twelve hardware/qcom-caf/msm8996/media
-     git clone https://github.com/afterallafk/hardware_qcom-caf_wlan.git -b twelve hardware/qcom-caf/wlan
-    
+         
     #clone_trees
      git clone https://github.com/afterallafk/voltage_device_xiaomi_onclite.git -b 12 device/xiaomi/onclite
      git clone https://github.com/afterallafk/vendor_xiaomi_onclite-12.git -b twelve vendor/xiaomi/onclite
